7f12745c52390d1d69af6e17015de87438c4e964
Mondo <mondogao@gmail.com>
2017-04-02 15:56:33 +0800

    feat(App): 增加路径变化的监听

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,27 @@ import {
 import './App.scss';
 
 import Nav from 'components/Nav'
-import HotPageContainer from 'containers/HotPageContainer.js'
+import HotPageContainer from 'containers/HotPageContainer'
 import MePageContainer from 'containers/MePageContainer'
 import UsersPageContainer from 'containers/UsersPageContainer'
 
-const App = (props) => (
-  <div>
-    <Route path="/" exact render={() => <HotPageContainer/>}/>
-    <Route path="/users" component={UsersPageContainer}/>
-    <Route path="/me" component={MePageContainer}/>
-    <Nav/>
-  </div>
-)
+class App extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      console.log('re-config wx')
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/" exact component={HotPageContainer}/>
+        <Route path="/users" component={UsersPageContainer}/>
+        <Route path="/me" component={MePageContainer}/>
+        <Nav/>
+      </div>
+    )
+  }
+}
 
 export default App;
