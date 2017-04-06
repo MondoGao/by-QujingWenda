6c92b821819a314d352f5b8436cc80ae31b1c6fd
Mondo <mondogao@gmail.com>
2017-04-06 12:11:59 +0800

    feat(components/Button): 增加大尺寸

--- src/components/UsersPage/index.js
+++ src/components/UsersPage/index.js
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import EntityList from 'components/EntityList'
 import UserItemContainer from 'containers/UserItemContainer'
 import UserMetaContainer from 'containers/UserMetaContainer'
+import Button from 'components/Button'
 
 class UsersPage extends React.Component {
   render() {
@@ -13,7 +14,7 @@ class UsersPage extends React.Component {
           <Route path={`/users/${this.props.myself.id}`}>
             <Redirect to="/me"/>
           </Route>
-          <Route path="/users/:id" render={({ match }) => <UserMetaContainer id={match.params.id} only/>}/>
+          <Route path="/users/:id" component={UserPage}/>
           <Route path="/users">
             <EntityList entityIds={this.props.page.list} entity={UserItemContainer} fullscreen/>
           </Route>
@@ -28,5 +29,14 @@ class UsersPage extends React.Component {
     }
   }
 }
+
+const UserPage = ({ match }) => (
+  <div>
+    <UserMetaContainer id={match.params.id} only/>
+    <div>
+      <Button size="lg">¥1 提个问题</Button>
+    </div>
+  </div>
+)
 
 export default UsersPage
