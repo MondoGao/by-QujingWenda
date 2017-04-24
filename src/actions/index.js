c022b8725b8b8f4ef42ee9d4e1a8a6725dbabdee
Mondo <mondogao@gmail.com>
2017-04-24 16:57:29 +0800

    docs(actions): 更新备注

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,11 @@ const asyncActionsCreator = (type, payload) => ({
   payload
 })
 
+/**
+ * {creator} 切换页面加载状态
+ * @param {string} page 页面常量
+ * @param {boolean} isLoading
+ */
 const togglePagesLoading = (page, isLoading) => ({
   type: consts[`UPDATE_PAGE_${page}_LOADING`],
   payload: {
@@ -13,13 +18,17 @@ const togglePagesLoading = (page, isLoading) => ({
   }
 })
 
+/**
+ * {thunk} 增加问题
+ * @return {Promise}
+ */
 export const appendQuestions = () => (dispatch, getState) => {
   const state = getState()
   const { page, isLoadingComplete } = state.pages.hot
 
   if (!isLoadingComplete) {
     dispatch(togglePagesLoading(consts.PAGES.HOT, true))
-    sources.getQuestions(page + 1)
+    return sources.getQuestions(page + 1)
       .then(normalizedData => {
         dispatch(asyncActionsCreator(consts.APPEND_QUESTIONS, normalizedData))
 
@@ -28,13 +37,17 @@ export const appendQuestions = () => (dispatch, getState) => {
   }
 }
 
+/**
+ * {thunk} 向后添加用户
+ * {Promise}
+ */
 export const appendUsers = () => (dispatch, getState) => {
   const state = getState()
   const { page, isLoadingComplete } = state.pages.users
 
   if (!isLoadingComplete) {
     dispatch(togglePagesLoading(consts.PAGES.USERS, true))
-    sources.getUsers(page + 1)
+    return sources.getUsers(page + 1)
       .then(normalizedData => {
         dispatch(asyncActionsCreator(consts.APPEND_USERS, normalizedData))
 
@@ -43,19 +56,30 @@ export const appendUsers = () => (dispatch, getState) => {
   }
 }
 
-export const appendUserQuestions = (id, filter, type) => (dispatch, getState) => {
+/**
+ * {thunk} 添加用户所属的问题
+ * @param {string|number} id 用户 id
+ * @param {asked|asking|paid} filter
+ * @param {1|2|3=} type 1 问我的|2 我问的|3 我听过
+ * @return {Promise}
+ */
+export const appendUserQuestions = (id, filter, type = 1) => (dispatch, getState) => {
   const state = getState()
   const prevPage = state.entities.users[id][filter] ? state.entities.users[id][filter].page : 0
 
-  sources.getUserQuestions(id, prevPage, type)
+  return sources.getUserQuestions(id, prevPage, type)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.APPEND_USER_QUESTIONS, {...normalizedData, type, filter, userId: id}))
     })
 }
 
+/**
+ * {thunk} 刷新首页问题列表
+ * @return {Promise}
+ */
 export const refreshQuestions = () => dispatch => {
   dispatch(togglePagesLoading(consts.PAGES.HOT, true))
-  sources.getQuestions(1)
+  return sources.getQuestions(1)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.REFRESH_QUESTIONS, normalizedData))
 
@@ -63,9 +87,13 @@ export const refreshQuestions = () => dispatch => {
     })
 }
 
+/**
+ * {thunk} 刷新用户页面列表
+ * @return {Promise}
+ */
 export const refreshUsers = () => dispatch => {
   dispatch(togglePagesLoading(consts.PAGES.HOT, true))
-  sources.getUsers(1)
+  return sources.getUsers(1)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.REFRESH_USERS, normalizedData))
 
@@ -73,6 +101,13 @@ export const refreshUsers = () => dispatch => {
     })
 }
 
+/**
+ * {thunk} 刷新用户所属的问题列表
+ * @param {string|number} id 用户 id
+ * @param {asked|asking|paid} filter
+ * @param {1|2|3=} type 1 问我的|2 我问的|3 我听过
+ * @return {Promise}
+ */
 export const refreshUserQuestions = (id, filter, type) => (dispatch, getState) => {
   const state = getState()
   const { myself } = state
@@ -87,7 +122,7 @@ export const refreshUserQuestions = (id, filter, type) => (dispatch, getState) =
     dispatch(togglePagesLoading(loadPage, true))
   }
 
-  sources.getUserQuestions(id, 1, type)
+  return sources.getUserQuestions(id, 1, type)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.REFRESH_USER_QUESTIONS, {...normalizedData, type, filter, userId: id}))
   
@@ -97,13 +132,23 @@ export const refreshUserQuestions = (id, filter, type) => (dispatch, getState) =
     })
 }
 
+/**
+ * {thunk} 刷新单个用户信息
+ * @param id
+ * @return {Promise}
+ */
 export const refreshUser = id => (dispatch, getState) => {
-  sources.getUser(id)
+  return sources.getUser(id)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.REFRESH_USER, normalizedData))
     })
 }
 
+/**
+ * {thunk} 刷新学校信息
+ * @param id
+ * @return {Promise}
+ */
 export const refreshSchool = id => (dispatch, getState) => {
   const { myself } = getState()
   
@@ -111,7 +156,7 @@ export const refreshSchool = id => (dispatch, getState) => {
     dispatch(togglePagesLoading(consts.PAGES.ME, true))
   }
   
-  sources.getSchool(id)
+  return sources.getSchool(id)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.REFRESH_SCHOOL, normalizedData))
       
@@ -122,17 +167,22 @@ export const refreshSchool = id => (dispatch, getState) => {
 }
 
 /**
- * 登陆并获取用户信息
+ * {thunk} 登陆并获取用户信息
  * @param {string} code 微信登陆 code
  * @return {Promise}
  */
 export const login = code => (dispatch, getState) => {
-  sources.login(code)
+  return sources.login(code)
     .then(normalizedData => {
       dispatch(asyncActionsCreator(consts.LOGIN_IN, normalizedData))
     })
 }
 
+/**
+ * {creator} 更新正在播放的音频
+ * @param id
+ * @return {action}
+ */
 export const updatePlayingAudio = id => ({
   type: consts.UPDATE_PLAYING_AUDIO,
   payload: {
@@ -140,6 +190,11 @@ export const updatePlayingAudio = id => ({
   }
 })
 
+/**
+ * {creator} 更新我的页面上个访问 tab
+ * @param {asked|asking|paid} lastTab
+ * @return {action}
+ */
 export const updateLastTab = lastTab => ({
   type: consts.UPDATE_PAGE_ME,
   payload: {
